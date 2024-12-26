fc78f98aba8404ebdb1133bfb7ad1ad466953fcd
bitsXbytes <12557052+bitsnorbytes@users.noreply.github.com>
2024-12-26 10:32:02 +0530

    Initial Commit

---
+++ Stream/StreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamApp.swift
+//  Stream
+//
+//  Created by Punya on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StreamTests/StreamTests.swift
@@ -0,0 +1,16 @@
+//
+//  StreamTests.swift
+//  StreamTests
+//
+//  Created by Punya on 26/12/24.
+//
+
+import Testing
+
+struct StreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
